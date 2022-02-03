a05ad0af441efe1cdb340a5718afafe1144f6a21
Jakub-C-7 <jakubchamera7@gmail.com>
2022-02-03 22:17:43 +0000

    Cleaned up includes

--- src/main/java/com/example/demo/feed/FeedController.java
+++ src/main/java/com/example/demo/feed/FeedController.java
@@ -1,26 +1,17 @@
 package com.example.demo.feed;
 
-import com.example.demo.appuser.AppUser;
-import com.example.demo.appuser.AppUserRepository;
 import com.example.demo.post.Post;
 import com.example.demo.post.PostService;
 import lombok.AllArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
-import java.time.Clock;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 //@RestController
 @Controller
