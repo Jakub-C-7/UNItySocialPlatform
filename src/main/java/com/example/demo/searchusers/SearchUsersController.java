8e5869a623e4b3c9da4e50002beb299523ed1b79
Jakub-C-7 <jakubchamera7@gmail.com>
2022-02-11 20:33:50 +0000

    Cleaned up code. Tested and all still working

--- src/main/java/com/example/demo/searchusers/SearchUsersController.java
+++ src/main/java/com/example/demo/searchusers/SearchUsersController.java
@@ -5,43 +5,26 @@ import com.example.demo.appuser.AppUserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * SearchUsersController Class provides routing to the searachusers page.
  *
- * Upon entering the search users page (GET request) the SearchUsersController calls the AppUserRepository to retrieve
- * all users and populates the page with results.
+ * If the searchusers page is entered without a search criteria input, all users are retrieved. If a search input is
+ * detected, the input it split into separate words (separated by spaces), and the AppUserRepository is called to
+ * search the database for users with a matching first or last name.
  *
  * @author jakub
  */
 @Controller
-//@RequestMapping("searchusers")
 @AllArgsConstructor
 public class SearchUsersController {
 
     private final AppUserRepository appUserRepository;
 
-//    @GetMapping
-//    public String getAllUsers(Model model) {
-//        List<AppUser> users = appUserRepository.findAll();
-//        model.addAttribute("users", users);
-//
-//        return "searchusers";
-//    }
-
-//    @GetMapping("{usersName}")
-
-//    public String findMatchingUser(@PathVariable("usersName") String usersName, Model model) {
-    @RequestMapping(path = {"/" , "/searchusers"})
+    @RequestMapping("/searchusers")
     public String findMatchingUser(String keyword, Model model) {
         if (!keyword.isEmpty()) {
 
@@ -49,15 +32,11 @@ public class SearchUsersController {
             if (nameArray.length == 2) {
 
                 List<AppUser> users = appUserRepository.findByFirstNameOrLastName(nameArray[0], nameArray[1]);
-                System.out.println(keyword);
                 model.addAttribute("users", users);
             } else {
                 List<AppUser> users = appUserRepository.findByFirstNameOrLastName(nameArray[0], nameArray[0]);
                 model.addAttribute("users", users);
-                System.out.println(keyword);
             }
-            System.out.println(keyword);
-
 
         } else {
             List<AppUser> users = appUserRepository.findAll();
@@ -68,38 +47,3 @@ public class SearchUsersController {
     }
 
 }
-
-
-//        //Removing the first few metadata characters from the search input
-//        String sanitisedUsersName = usersName.substring(4, usersName.length());
-//
-//        //Splitting the sanitised input where there is a + sign
-//        String [] nameArray = sanitisedUsersName.split("\\+");
-//
-//
-//        //If a first and last name have been entered
-//        if (nameArray.length == 2){
-//            List<AppUser> users = appUserRepository.findByFirstNameOrLastName(nameArray[0], nameArray[1]);
-//
-//            model.addAttribute("users", users);
-//
-//            return "redirect:/searchusers/{sanitisedUsersName}";
-//
-//        } else {
-//            String [] singleNameArray = usersName.split("=");
-//
-//            String search = singleNameArray[1];
-//
-//            List<AppUser> users = appUserRepository.findByFirstNameOrLastName(search, search);
-//
-//            model.addAttribute("users", users);
-//
-//            return "redirect:/searchusers/{search}";
-
-//        }
-
-//        return "redirect:/searchusers";
-
-//    }
-
-//}
