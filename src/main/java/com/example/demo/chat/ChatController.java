5884a1874a1ea70fe229db11f58a3c28a5f3e8bc
Jakub-C-7 <jakubchamera7@gmail.com>
2022-02-28 22:41:39 +0000

    If chat already exists, redirects user to that chat. Added default mapping for the "/chat" path which redirects to the messageinbox.html. If the person attempting to see the chat isn't a participant, redirect to their messageinbox.html.

--- src/main/java/com/example/demo/chat/ChatController.java
+++ src/main/java/com/example/demo/chat/ChatController.java
@@ -2,15 +2,21 @@ package com.example.demo.chat;
 
 import com.example.demo.appuser.AppUser;
 import com.example.demo.appuser.AppUserRepository;
+import com.example.demo.chatMessage.ChatMessage;
+import com.example.demo.chatMessage.ChatMessageRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 /**
- * ChatController Class provides the routing of business logic for specific chat application functions.
+ * ChatController Class provides the routing of business logic for chat application functions.
  *
  * Creating, editing, deleting chats.
  *
@@ -18,14 +24,36 @@ import java.security.Principal;
  */
 @Controller
 @AllArgsConstructor
+@RequestMapping("chat")
 public class ChatController {
 
     AppUserRepository appUserRepository;
     ChatRepository chatRepository;
     ChatService chatService;
+    ChatMessageRepository chatMessageRepository;
+
+    @GetMapping
+    public String defaultRoute (){
+        return "redirect:/messageinbox";
+    }
+
+    @GetMapping(path = "{id}")
+    public String getChat(@PathVariable Long id, Principal principal, Model model){
+        Chat chat = chatRepository.findById(id).get();
+
+        if (!chat.getParticipantOne().getEmail().equals(principal.getName()) && !chat.getParticipantTwo().getEmail().equals(principal.getName())){
+            return "redirect:/messageinbox";
+        }
+                List<ChatMessage> messages = chatMessageRepository.findByChat(chat);
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("chat", chat);
+
+        return "chat";
+    }
 
     @PostMapping(path = "createnewchat/{id}")
-    public String newPost(@PathVariable Long id, Principal principal) {
+    public String newChat(@PathVariable Long id, Principal principal) {
 
         AppUser currentUser = appUserRepository.getById(
                 appUserRepository.findByEmail(principal.getName()).get().getId());
@@ -35,10 +63,15 @@ public class ChatController {
         Boolean chatAlreadyExists1 = !chatRepository.findByParticipantOneAndParticipantTwo(currentUser, otherUser).isEmpty();
         Boolean chatAlreadyExists2 = !chatRepository.findByParticipantOneAndParticipantTwo(otherUser, currentUser).isEmpty();
 
-        if (chatAlreadyExists1 || chatAlreadyExists2) {
-            //TODO: Redirect to the existing chat
-            // return "redirect:/messageinbox/{id}"
-            return "redirect:/messageinbox";
+        //If chat already exists, redirect to that chat.
+        if (chatAlreadyExists1) {
+            Long chatId = chatRepository.findByParticipantOneAndParticipantTwo(currentUser, otherUser).get(0).getId();
+
+             return "redirect:/chat/" + chatId;
+        } else if (chatAlreadyExists2) {
+            Long chatId =  chatRepository.findByParticipantOneAndParticipantTwo(otherUser, currentUser).get(0).getId();
+
+            return "redirect:/chat/" + chatId;
         }
 
         chatService.createNewChat(currentUser, otherUser);
