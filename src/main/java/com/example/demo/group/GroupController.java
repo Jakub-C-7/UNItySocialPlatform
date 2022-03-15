dc01bce15f903301d6fcc7e1a188c0d19d269fac
Jakub-C-7 <jakubchamera7@gmail.com>
2022-03-15 18:11:58 +0000

    Separated groups.html from group.html for easier separation of viewing groups. Enabled the viewing of created groups on their own page.

--- src/main/java/com/example/demo/group/GroupController.java
+++ src/main/java/com/example/demo/group/GroupController.java
@@ -2,16 +2,14 @@ package com.example.demo.group;
 
 import com.example.demo.appuser.AppUser;
 import com.example.demo.appuser.AppUserRepository;
+import com.example.demo.groupMember.GroupMember;
+import com.example.demo.groupMember.GroupMemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.util.List;
 
 /**
  * GroupController Class provides routing to the groups page.
@@ -19,27 +17,74 @@ import java.util.List;
  * @author jakub
  */
 @Controller
-@RequestMapping("groups")
+@RequestMapping("group")
 @AllArgsConstructor
 public class GroupController {
 
     private final GroupService groupService;
+    private final GroupRepository groupRepository;
+    private final GroupMemberRepository groupMemberRepository;
     private final AppUserRepository appUserRepository;
 
     @GetMapping
-    public String getAllGroups(Model model) {
-        List<AppGroup> groups = groupService.getGroups();
-        model.addAttribute("groups", groups);
+    public String defaultRoute() {
 
-        return "groups";
-    }
-
-    @PostMapping
-    public String createGroup(Principal principal, @RequestParam String name, @RequestParam String description, @RequestParam String type){
-        AppUser user = appUserRepository.findByEmail(principal.getName()).get();
-
-        groupService.createGroup(user, name, description, type);
 
         return "redirect:/groups";
+    }
+
+    @GetMapping("{groupId}")
+    public String getUserProfile(@PathVariable("groupId") Long groupId, Model model, Principal principal){
+        boolean groupExists = groupRepository.findById(groupId).isPresent();
+
+        if (!groupExists){
+            return "redirect:/groups";
+        }
+
+        AppGroup group = groupRepository.getById(groupId);
+
+        if (principal != null) {
+            AppUser loggedInUser = appUserRepository.findByEmail(principal.getName()).get();
+            GroupMember record = groupMemberRepository.findByGroupAndUser(group, loggedInUser);
+
+            Boolean isAMember;
+            Boolean memberPending;
+            if (record != null && record.getAdded() == true){
+                isAMember = true;
+
+            } else if (record != null && record.getAdded() == false){
+                memberPending = true;
+                isAMember = false;
+            }
+            else {
+                isAMember = false;
+            }
+
+
+//            //Check to see if the current user is a member
+//            if (groupMemberRepository.findByGroupAndUser(group, loggedInUser) != null){
+//                isAMember = true;
+//                friendRecord = friendRepository.findByUserAndUsersFriend(loggedInUser, user).get(0);
+//                model.addAttribute("friendRecord", friendRecord);
+//
+//            } else if (!friendRepository.findByUserAndUsersFriend(user, loggedInUser).isEmpty()) {
+//                isAMember = true;
+//                friendRecord = friendRepository.findByUserAndUsersFriend(user, loggedInUser).get(0);
+//                model.addAttribute("friendRecord", friendRecord);
+//            }
+//
+//            else {
+//                isAMember = false;
+//            }
+//            model.addAttribute("isAMember", isAMember);
+//
+        }
+
+        model.addAttribute("group", group);
+
+//        List<Post> posts = postService.getPostsByUserId(groupId);
+//        model.addAttribute("posts", posts);
+
+        return "group";
     }
 }
---
+++ src/main/java/com/example/demo/groups/GroupsController.java
@@ -0,0 +1,47 @@
+package com.example.demo.groups;
+
+import com.example.demo.appuser.AppUser;
+import com.example.demo.appuser.AppUserRepository;
+import com.example.demo.group.AppGroup;
+import com.example.demo.group.GroupService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.List;
+
+/**
+ * GroupController Class provides routing to the groups page.
+ *
+ * @author jakub
+ */
+@Controller
+@RequestMapping("groups")
+@AllArgsConstructor
+public class GroupsController {
+
+    private final GroupService groupService;
+    private final AppUserRepository appUserRepository;
+
+    @GetMapping
+    public String getAllGroups(Model model) {
+        List<AppGroup> groups = groupService.getGroups();
+        model.addAttribute("groups", groups);
+
+        return "groups";
+    }
+
+    @PostMapping
+    public String createGroup(Principal principal, @RequestParam String name, @RequestParam String description, @RequestParam String type){
+        AppUser user = appUserRepository.findByEmail(principal.getName()).get();
+
+        groupService.createGroup(user, name, description, type);
+
+        return "redirect:/groups";
+    }
+}
